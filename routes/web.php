a8106bc417afeb60e404daac282424cb367faded
Farid <faridqasimov8@gmail.com>
2024-07-29 16:48:16 +0400

    prefix

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,19 @@
 use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CartController::class,"allItems"]);
 
-Route::prefix('/cart')->name('.basket')->group(function(){
+Route::prefix('cart')->name('.cart')->group(function(){
     Route::get('/all',[CartController::class,'allItems']);
+    Route::get('/{productId}',[CartController::class,'get']);
+    Route::post('/add/{productId}',[CartController::class,'add']);
+    Route::post('/update/{productId}',[CartController::class,'update']);
+    Route::get('/destroy',[CartController::class,'destroy']);
+    Route::get('/total',[CartController::class,'total']);
+    Route::get('/tax',[CartController::class,'tax']);
+    Route::post('/create/tax',[CartController::class,'setTax']);
+    Route::get('/tax',[CartController::class,'tax']);
+    Route::get('/subTotal',[CartController::class,'subTotal']);
+    Route::get('/count',[CartController::class,'count']);
+    Route::get('/delete/{productId}',[CartController::class,'delete']);
+
 });
